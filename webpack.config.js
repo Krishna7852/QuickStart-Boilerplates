d7579ba6a99302feb5c8e9dfb519e873aa25aa30
Bhamare Krushna <krushnabhamare@gmail.com>
2020-11-13 12:43:31 +0530

    Initial commit for angular multi-portal app

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.html", "./src/**/*.component.ts"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = (config, options) => {
+  console.log(`Using '${config.mode}' mode`);
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      {
+        loader: "postcss-loader",
+        options: {
+          ident: "postcss",
+          syntax: "postcss-scss",
+          plugins: [
+            require("tailwindcss")("./tailwind.config.js"),
+            require("autoprefixer"),
+            ...(config.mode === "production" ? [purgecss] : []),
+          ],
+        },
+      },
+    ],
+  });
+  return config;
+};
