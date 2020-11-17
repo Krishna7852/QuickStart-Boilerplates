2621aa5c49d3d07e1ad2b2806dbd51a546203deb
Bhamare Krushna <krushnabhamare@gmail.com>
2020-11-17 15:26:17 +0530

    change: separated dev-dependancy

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 import { createStore, applyMiddleware } from "redux";
 import { logger } from "redux-logger";
 import { persistStore } from "redux-persist";
